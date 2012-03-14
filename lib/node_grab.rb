5673709b0f46877debed5ea1d986c64889ae72fa
Matteo Picciolini <matteo.picciolini@gmail.com>
2012-03-14 18:06:07 +0100

    switch to ruby 1.9

---
+++ lib/node_grab.rb
@@ -0,0 +1,61 @@
+require 'rubygems'
+require 'drb'
+require 'sys/cpu'
+require 'open3'
+require 'facter'
+include Sys
+
+
+#fix a name client for test 
+
+class GrabNode
+	def initialize
+		Facter.loadfacts
+		@cpu = Facter.processorcount.to_i
+	end
+	
+	def grab_job
+		DRb.start_service
+		queue = DRbObject.new_with_uri('druby://127.0.0.1:61676')
+		puts 'Listening for connection ...'
+        	while job = queue.deq
+			if job["request"][:cpu].to_i<=@cpu
+				exec_job(job)
+			end
+        	end
+	end
+
+	def exec_job(job)
+		@stdin, @stdout, @stderr,@wait_thr = Open3.popen3(job["request"][:command])
+		puts "here"
+		puts @wait_thr.value
+		puts @wait_thr.value.class
+		puts @wait_thr.value.pid
+		puts @wait_thr.value.success?
+		write_a_file(job)
+		#puts "----------------------------"
+		#puts @output.readlines
+		#puts "----------------------------"
+		#puts @error.readlines
+	end
+
+	def write_a_file(job)
+		Dir.chdir
+		begin 
+			Dir.mkdir 'rqreport'
+		rescue Exception
+		end
+		Dir.chdir 'rqreport'
+		File.open(job["request"][:id].to_s+'_'+'out.txt', 'w') do |write|
+			write.puts @stdout.readlines
+		end
+		File.open(job["request"][:id].to_s+'_'+'err.txt', 'w') do |write|
+                        write.puts @stderr.readlines
+                end
+
+	end
+
+end
+
+GrabNode.new.grab_job
+
--- lib/rqueue_server.rb
+++ lib/rqueue_server.rb
@@ -1,6 +1,5 @@
-require "rubygems"
+require 'rubygems'
 require 'drb'
-require 'net/ssh'
 
 require 'setup'
 require 'wise'
@@ -9,11 +8,10 @@ require 'wise'
 setup = Setup.new
 setup.make_table
 setup.make_parser_machine
-setup.make_parser_job
 @sage = Wise.new
 
 
-def run_queue(url='druby://127.0.0.1:9001')
+def run_queue(url='druby://127.0.0.1:61676')
 	queue = Queue.new
 	DRb.start_service(url,queue)
 	jobs = Array.new
@@ -23,57 +21,13 @@ def run_queue(url='druby://127.0.0.1:9001')
 			queue.enq("request"=>jo)
 		end 
 	end
-	#start code
-	while job = queue.deq
-                yield job
-        end
+	#to do sostiutirlo con un 
+	#demone
+	while true 
+	end
 end
 
  
-def mind (job,job_cpu,t)
 	
-	#sceglie la macchina con il minor spreco di cpu
-	b = @sage.greedy(job_cpu) 
-        # chimata ssh del comando con grep dei parametri
-	puts "in ssh"
-	puts b[:ip]
-	puts b[:ssh_user]
-	puts b[:ssh_pass]
-	puts job[:command]
-	Net::SSH.start(b[:ip], b[:ssh_user], :password => b[:ssh_pass] ) do |ssh|
-        ssh.exec job[:command] do |ch, stream, data|
-         if stream == :stderr
-                 puts "ERROR: #{data}"
-		 # put the job into error table
-		 a.persistence_with_error(job,"Job fail","ERROR: #{data}")
-		 t.kill
-         else
-                 puts data
-		 t.kill
-         end
-        end
-	end
-	time_to_kill = b.start_date+(60*60*b.h_request)
-	while true do
-		 if Time.now == time_to_kill
-			t.kill 
-			break
-	end
-	
-end
-			
+run_queue()			
 
-end
-
-run_queue do |job|
-		puts "in block"	
-		a_job = job["request"]
-		job_cpu = a_job [:cpu]
-                
-		t=Thread.new{mind(a_job,job_cpu,self)}
-                t.join
-        end
-
-	
-	
-	
